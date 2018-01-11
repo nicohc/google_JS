b0c7ca245a8fd0a7687cbab164ea3ab957d39391
Nico <nico1099@hotmail.fr>
2018-01-11 18:11:14 +0100

    Etape 2 finie

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
+// Exo 1
+
 var lebouton = document.querySelector('button');
 var lecontenu = document.getElementById("elementscentraux");
-
 
 function affichage() {
     if (lecontenu.style.display === "none") {
@@ -14,4 +15,18 @@ function affichage() {
 
 lebouton.onclick = function() {
   affichage();
+}
+
+// Exo 2
+
+
+var boutonrecherche = document.getElementById("bouton1");
+
+function recherche(larecherche) {
+    var larecherche = document.getElementById("elementrecherche").value;
+    window.location = "http://www.google.fr/search?q=" + larecherche;
+}
+
+bouton1.onclick = function () {
+  recherche();
 }
